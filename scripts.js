621b6815823ddc169c06f94b28c6cd73248a5289
BIR-RTAF <vir.bahos@gmail.com>
2025-05-19 22:22:16 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,29 +10,6 @@ document.getElementById('womacForm').addEventListener('submit', function (e) {
   radioFields.forEach((radio) => {
     totalScore += parseInt(radio.value);
     maxScore += 4;
-
-    // เปลี่ยนสีของปุ่มคำตอบตามคะแนนที่เลือก
-    const label = radio.nextElementSibling;  // หาป้าย label ที่อยู่ถัดจาก radio button
-    label.style.backgroundColor = "";  // รีเซ็ตสีพื้นหลังเก่า
-
-    // กำหนดสีตามค่าที่เลือก
-    switch (radio.value) {
-      case '0':
-        label.style.backgroundColor = '#a5d6a7'; // สีฟ้า
-        break;
-      case '1':
-        label.style.backgroundColor = '#81c784'; // สีเขียว
-        break;
-      case '2':
-        label.style.backgroundColor = '#fff176'; // สีเหลือง
-        break;
-      case '3':
-        label.style.backgroundColor = '#ffb74d'; // สีส้ม
-        break;
-      case '4':
-        label.style.backgroundColor = '#e57373'; // สีแดง
-        break;
-    }
   });
 
   const percent = ((totalScore / maxScore) * 100).toFixed(2);
